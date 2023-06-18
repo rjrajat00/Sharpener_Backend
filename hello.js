d09b6711eefab2a759d1f04885745c1c90a7aac1
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2023-06-18 18:19:47 +0530

    creating server

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  // Log your name
+  res.write("Rajat  ");
+
+  res.write("  hello sharpner this is node js");
+
+  // Send the response
+  res.end();
+});
+
+server.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
