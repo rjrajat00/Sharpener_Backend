bd7a6b4296fc50b4ce5062a25a24487b0d789fbb
Rajat SIngh <rjrajat1003@gmail.com>
2023-06-21 18:39:18 +0530

    Writing and reading from a file

--- hello.js
+++ hello.js
@@ -1,42 +1,48 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  // Log your name
-
   let url = req.url;
+  let method = req.method;
 
-  if (url === "/home") {
-    res.write("<html>");
-
-    res.write("<body><h1 style = 'red'>Welcome to Home  Page </h1> </body> ");
-
-    res.write("</html>");
-    return res.end();
+  if (url === "/") {
+    if (method === "POST") {
+      const body = [];
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log("parsedBody==>", parsedBody);
+        const message = parsedBody.split("=")[1];
+        fs.writeFile("message.txt", message, (err) => {
+          if (err) {
+            console.log(err);
+          }
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        });
+      });
+    } else {
+      fs.readFile("message.txt", "utf8", (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        res.write("<html>");
+        res.write("<head><title>Enter The Message</title></head>");
+        res.write(
+          `<body><form action="/" method="POST">
+            <input type="text" name="message" placeholder="enter your message">
+            <button type="submit">Send</button>
+          </form>`
+        );
+        res.write(`<p> ${data}</p>`);
+        res.write("</body></html>");
+        res.end();
+      });
+    }
   }
-
-  if (url === "/about") {
-    res.write("<html>");
-
-    res.write("<body><h1 style = 'red'>Welcome to About Us </h1> </body> ");
-
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/node") {
-    res.write("<html>");
-
-    res.write(
-      "<body><h1 {style :red} >Welcome to MY Node JS Project </h1> </body> "
-    );
-
-    res.write("</html>");
-    return res.end();
-  }
-
-  // Send the response
-  res.end();
 });
 
 server.listen(4000, () => {
