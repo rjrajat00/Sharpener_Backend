55e3d5dee2d93d758b302d3d62252fd33578b9e5
Rajat SIngh <rjrajat1003@gmail.com>
2023-06-24 10:30:36 +0530

    Router, filtering paths, 404 error

--- hello.js
+++ hello.js
@@ -1,28 +1,20 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("first mW>");
-  res.send(
-    "<html><body>   <h2>  Add product page </h2> <form action='/product' method='POST'> <input type='text' name='title' placeholder='name' >  <input type='text' name='size' placeholder='size' > <button type='submit'>Add Product</button></form></body></html>"
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  const { title, size } = req.body;
-  console.log("Product title:", title);
-  console.log("Product size:", size);
-
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h2>hello from my express app</h2>");
+app.use((req, res, next) => {
+  res.status(404).send("<html><h1>404 Error: Page Not Found</h1></html>");
 });
 
 app.listen(5000, () => {
